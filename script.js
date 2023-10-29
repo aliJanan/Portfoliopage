e3ab137d809493c453185c66bee95fc2bc4b2a77
ali <aliy.janan@gmail.com>
2023-10-29 21:34:25 +0100

    doing some codings and some changes

--- script.js
+++ script.js
@@ -14,20 +14,29 @@ window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
 
 
+/** HEADER | active header when window scroll down to 100px */
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
 
 
 
 
-
-  /* AUTO TYPE JS START */
-  document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed(".auto-type", {
-      strings: ["Web Designer", "Frontend Developer", "Backend Developer", "UI/UX Designer"],
-      loop: true,
-      typeSpeed: 150,
-      backSpeed: 150
-    });
+/* AUTO TYPE JS START */
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed(".auto-type", {
+    strings: ["Web Designer", "Frontend Developer", "Backend Developer", "UI/UX Designer"],
+    loop: true,
+    typeSpeed: 150,
+    backSpeed: 150
   });
-  /* AUTO TYPE JS END */
+});
+/* AUTO TYPE JS END */
 
 
